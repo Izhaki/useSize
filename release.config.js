b33b971b0ef15a157a7a441b9c284269a2ad78e1
Izhaki <roey@gefri.org>
2021-07-17 21:41:50 +0100

    fix: package.json version on root not bumped upon release

--- release.config.js
+++ release.config.js
@@ -1,4 +1,25 @@
 module.exports = {
   pkgRoot: 'dist',
   branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'npx rjp package.json version ${nextRelease.version}',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+  ],
 };
